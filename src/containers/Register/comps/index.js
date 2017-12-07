fcccc09e63c94a5d2e8a36d0da162cccee4986c3
Ba5eem <baseem@gmail.com>
2017-12-07 11:52:30 -1000

    req setup

--- src/containers/Register/comps/index.js
+++ src/containers/Register/comps/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 
 const RegisterComponent = ({usernameHandler,passwordHandler,submit,username,password}) =>{
+  const greenCheck = "http://bit.ly/2jqOoqa";
+  const redCross = "http://bit.ly/2nGi1IM";
+
+  const usernameReqOne = true ? greenCheck : redCross;
+  const usernameReqTwo = false ? greenCheck : redCross;
+  const usernameReqThree = true ? greenCheck : redCross;
+
+  const passwordReqOne = false ? greenCheck : redCross;
+  const passwordReqTwo = true ? greenCheck : redCross;
+  const passwordReqThree = true ? greenCheck : redCross;
+
+  const emailReqOne = false ? greenCheck : redCross;
+  const emailReqTwo = false ? greenCheck : redCross;
+  const emailReqThree = true ? greenCheck : redCross;
+
+
+
   return (
       <div className="RegisterContainer">
         <form className="RegisterContainer" type="onsubmit" onSubmit={submit}>
-
+{/*USERNAME*/}
           <div className="usernameDiv">
             <h3 className='UsernameHeading'>USERNAME</h3>
             <input 
@@ -16,9 +33,26 @@ const RegisterComponent = ({usernameHandler,passwordHandler,submit,username,pass
           </div>
 
           <div className="usernameDivReqs">
-
+            <div className="reqDiv">
+              <img 
+                className="greenCheck" 
+                src={usernameReqOne} 
+                alt="null"/>Username req One
+            </div>
+            <div className="reqDiv">
+              <img 
+                className="greenCheck" 
+                src={usernameReqTwo} 
+                alt="null"/>Username req One
+            </div>
+            <div className="reqDiv">
+              <img 
+                className="greenCheck" 
+                src={usernameReqThree} 
+                alt="null"/>Username req One
+            </div>
           </div>
-
+{/*EMAIL*/}
           <div className="emailDiv">
             <h3 className='EmailHeading'>EMAIL</h3>
             <input 
@@ -29,9 +63,27 @@ const RegisterComponent = ({usernameHandler,passwordHandler,submit,username,pass
           </div>
 
           <div className="emailDivReqs">
+            <div className="reqDiv">
+              <img 
+                className="greenCheck" 
+                src={emailReqOne} 
+                alt="null"/>Email req One
+            </div>
+            <div className="reqDiv">
+              <img 
+                className="greenCheck" 
+                src={emailReqTwo} 
+                alt="null"/>Email req One
+            </div>
+            <div className="reqDiv">
+              <img 
+                className="greenCheck" 
+                src={emailReqThree} 
+                alt="null"/>Email req One
+            </div>
 
           </div>
-
+{/*PASSWORD*/}
           <div className="passwordDiv">
             <h3 className="PasswordHeading">PASSWORD</h3>
             <input
@@ -42,9 +94,27 @@ const RegisterComponent = ({usernameHandler,passwordHandler,submit,username,pass
           </div>
 
           <div className="passwordDivReqs">
+            <div className="reqDiv">
+              <img 
+                className="greenCheck" 
+                src={passwordReqOne} 
+                alt="null"/>Password req One
+            </div>
+            <div className="reqDiv">
+              <img 
+                className="greenCheck" 
+                src={passwordReqTwo} 
+                alt="null"/>Password req One
+            </div>
+            <div className="reqDiv">
+              <img 
+                className="greenCheck" 
+                src={passwordReqThree} 
+                alt="null"/>Password req One
+            </div>
 
           </div>
-
+{/*SUBMIT*/}
           <div className="submitDiv">
             <input 
               type='submit'
