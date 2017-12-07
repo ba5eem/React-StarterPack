fb35c8fbdc0e0ce3880b03fa26da74105aee1058
Ba5eem <baseem@gmail.com>
2017-12-07 11:31:27 -1000

    register form building

--- src/containers/Register/comps/index.js
+++ src/containers/Register/comps/index.js
@@ -4,29 +4,59 @@ import React from 'react';
 const RegisterComponent = ({usernameHandler,passwordHandler,submit,username,password}) =>{
   return (
       <div className="RegisterContainer">
-        <h3 className='RegisterHeading'>Register</h3>
-        <form type="onsubmit" onSubmit={submit}>
-          <input 
-            onChange={usernameHandler}
-            value={username}
-            className="RegisterInput">
-          </input>
+        <form className="RegisterContainer" type="onsubmit" onSubmit={submit}>
 
-          <h3 className="PasswordHeading">PASSWORD</h3>
+          <div className="usernameDiv">
+            <h3 className='UsernameHeading'>USERNAME</h3>
+            <input 
+              onChange={usernameHandler}
+              value={username}
+              className="UsernameInput">
+            </input>
+          </div>
 
-          <input
-            onChange={passwordHandler} 
-            value={password}
-            className="PasswordInput">
-          </input>
+          <div className="usernameDivReqs">
 
-          <input 
-            type='submit'
-            value='SUBMIT'
-            className="RegisterSubmit">
-          </input>
+          </div>
+
+          <div className="emailDiv">
+            <h3 className='EmailHeading'>EMAIL</h3>
+            <input 
+              onChange={usernameHandler}
+              value={username}
+              className="EmailInput">
+            </input>
+          </div>
+
+          <div className="emailDivReqs">
+
+          </div>
+
+          <div className="passwordDiv">
+            <h3 className="PasswordHeading">PASSWORD</h3>
+            <input
+              onChange={passwordHandler} 
+              value={password}
+              className="PasswordInput">
+            </input>
+          </div>
+
+          <div className="passwordDivReqs">
+
+          </div>
+
+          <div className="submitDiv">
+            <input 
+              type='submit'
+              value='SUBMIT'
+              className="RegisterSubmit">
+            </input>
+          </div>
+
+          <div className="submitDivReqs">
+
+          </div>
         </form>
-
       </div>
 
 
