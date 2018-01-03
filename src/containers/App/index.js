780d76df25d16aeb5749c64c15700d050a5dd8a3
Ba5eem <baseem@gmail.com>
2018-01-02 16:50:27 -1000

    dnd image to 9

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import loadData from '../../actions';
 
-
+const dropHere = "http://bit.ly/2CH7JMA";
+const arr = [
+{id:"0", src: dropHere},
+{id:"1", src: dropHere},
+{id:"2", src: dropHere},
+{id:"3", src: dropHere},
+{id:"4", src: dropHere},
+{id:"5", src: dropHere},
+{id:"6", src: dropHere},
+{id:"7", src: dropHere},
+{id:"8", src: dropHere}]
 
 
 class App extends Component {
@@ -10,8 +20,14 @@ class App extends Component {
     super();
     
     this.state={ 
-      data: []
+      arr: arr,
+      url: ''
     }
+
+    this.onDrop=this.onDrop.bind(this);
+    this.drag=this.drag.bind(this);
+    this.onDragOver=this.onDragOver.bind(this);
+
   }
 
 
@@ -19,44 +35,77 @@ class App extends Component {
     this.props.loadData();
   }
 
-  renderPhotos(elem,i){
-    let {url} = elem.images.low_resolution;
-    let {width} = elem.images.low_resolution;
-    let {height} = elem.images.low_resolution;
-    return(
-        <div className="ig_list" key={i}>
-          <img style={{width:width, height: height}} src={url} alt=""/>
-        </div>
-      )
+  drag(e,url){
+    this.setState({url})
   }
 
-  renderBoxes(elem,i){
-    return (
-        <div className="nine_boxes" key={i}>
-          <img src="http://bit.ly/2CH7JMA" alt="" className="drophere"/>
-        </div>
-      )
+  onDrop(e,idx){
+    let {arr} = this.state;
+    let res = arr.filter((elem,i) => {
+      if(parseInt(elem.id) === parseInt(idx)){
+        elem.src = this.state.url;
+      }
+      return arr;
+    })
+    this.setState({arr:res})
   }
 
+  onDragOver(e){
+    e.preventDefault();
+  }
+
+
+  
 
 
 
 
   render(){
     let {data} = this.props.data;
-    let arr = new Array(9).fill('i');
+    let {arr} = this.state;
+
+    
     return (
 
         <div className="App">
          <div className="side_section">
-          {data.map(this.renderPhotos)}
+          {data.map((elem,i) =>{
+            let {url} = elem.images.low_resolution;
+            let {width} = elem.images.low_resolution;
+            let {height} = elem.images.low_resolution;
+            return(
+              <div 
+                url={url} 
+                className="ig_list" 
+                key={i} 
+                draggable='true' 
+                onDrag={(e)=>this.drag(e,url)}
+                onDragOver={(e)=>this.onDragOver(e)} 
+                onDrop={(e)=>this.onDrop(e,url)}>
+
+                <img style={{width:width, height: height}} src={url} alt=""/>
+              </div>
+              )
+          })}
          </div>
          <div className="main">
           <div className="header">
             <h1>My Favorite 9</h1>
           </div>
           <div className="template">
-            {arr.map(this.renderBoxes)}
+            {arr.map((elem,i) =>{
+              return (
+                <div 
+                  className="nine_boxes"
+                  key={i} 
+                  draggable='false'
+                  onDragOver={(e)=>this.onDragOver(e,i)} 
+                  onDrop={(e)=>this.onDrop(e,i)}>
+
+                  <img src={elem.src} alt="" className="drophere"/>
+                </div>
+                )
+            })}
           </div>
          </div>
           
