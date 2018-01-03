b15f83a0a327131808837bdc97cb42c56efa2449
Ba5eem <baseem@gmail.com>
2018-01-02 14:18:32 -1000

    updated

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,27 @@
 import { FETCHING_DATA, FETCHING_DATA_SUCCESS,FETCHING_DATA_FAIL  } from './../Utils/ActionTypes';
+import { token } from './token.js';
+
+
 
 const axios = require('axios');
-const Promise = require('bluebird');
+const uri = `https://api.instagram.com/v1/users/self/media/recent/?access_token=${process.env.IG_TOKEN}`
 
 
 
 
 export const loadData = () => {
-  return Promise.coroutine(function* (dispatch) {
-    dispatch({ 
-      type: FETCHING_DATA, 
-      payload: yield axios.get('/api/data') 
-    })
-  })
+  console.log('loadusers to server ' )
+  return function(dispatch){
+    return axios.get(uri)
+    .then( users => {
+      console.log('loadusers? ', users )
+      dispatch({
+        type: FETCHING_DATA,
+        payload: users.data
+      });
+    });
+  }
 }
+
 
 
--- src/reducers/tempList.js
+++ src/reducers/tempList.js
@@ -13,6 +13,7 @@ export default function (state = initialState, action) {
   switch(action.type){
 
     case FETCHING_DATA:
+      console.log(action.payload);
       return Object.assign({},state, {
         isFetching: true,
         data: null,
